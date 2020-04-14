1df0615456f0cbf50f346e85ea09613a5880e394
Tim Godfrey <tim@webnerd.io>
2020-04-14 15:36:01 +0200

    Lesson 9: Universal mode deployment

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3001`,
+  baseURL: `https://my-json-server.typicode.com/timmygee/vuemastery-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
